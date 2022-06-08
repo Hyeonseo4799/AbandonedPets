8a6fd1a6046cdb5d0f34ea4f97fc20daca64c572
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-08 18:24:15 +0900

    요청할 BASE_URL 변경

--- data/src/main/java/com/project/data/utils/Utils.kt
+++ data/src/main/java/com/project/data/utils/Utils.kt
@@ -1,5 +1,5 @@
 package com.project.data.utils
 
 object Utils {
-    const val BASE_URL = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic"
+    const val BASE_URL = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/"
 }
