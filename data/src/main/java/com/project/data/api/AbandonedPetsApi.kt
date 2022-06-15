44240e35c18b7b60d4ca06b92fba8a384edb5daf
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-15 15:57:10 +0900

    Call 대신 Response 사용

--- data/src/main/java/com/project/data/api/AbandonedPetsApi.kt
+++ data/src/main/java/com/project/data/api/AbandonedPetsApi.kt
@@ -2,18 +2,18 @@ package com.project.data.api
 
 import com.project.data.BuildConfig
 import com.project.data.model.AbandonedPetsResponse
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface AbandonedPetsApi {
     @GET("abandonmentPublic")
-    fun getInfo(
+    suspend fun getInfo(
         @Query("bgnde") bgnde: String,
         @Query("endde") endde: String,
         @Query("pageNo") pageNo: String,
         @Query("numOfRows") numOfRows: String,
         @Query("_type") type: String = "json",
-        @Query("serviceKey") serviceKey: String = BuildConfig.API_KEY
-    ): Call<AbandonedPetsResponse>
+        @Query(value="serviceKey", encoded = true) serviceKey: String = BuildConfig.API_KEY
+    ): Response<AbandonedPetsResponse>
 }
