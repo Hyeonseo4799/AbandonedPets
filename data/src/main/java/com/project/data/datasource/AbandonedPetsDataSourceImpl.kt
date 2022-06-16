8c86e708a4713c7fe685f51014e939462bf0cf1c
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-16 17:27:08 +0900

    Change Type Not Null

--- data/src/main/java/com/project/data/datasource/AbandonedPetsDataSourceImpl.kt
+++ data/src/main/java/com/project/data/datasource/AbandonedPetsDataSourceImpl.kt
@@ -2,10 +2,10 @@ package com.project.data.datasource
 
 import com.project.data.api.AbandonedPetsApi
 import com.project.data.model.AbandonedPetsResponse
-import com.project.data.utils.BaseRepository
+import com.project.data.utils.base.BaseRepository
 
 class AbandonedPetsDataSourceImpl(private val api: AbandonedPetsApi) : BaseRepository(), AbandonedPetsDataSource {
-    override suspend fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): AbandonedPetsResponse? {
-        return safeApiCall() { api.getInfo(bgnde, endde, pageNo, numOfRows).body() }
+    override suspend fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): AbandonedPetsResponse {
+        return safeApiCall { api.getInfo(bgnde, endde, pageNo, numOfRows).body() }!!
     }
 }
