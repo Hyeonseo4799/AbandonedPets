696e904ee19689d6ab00e63d2cf8187cc22dfdc9
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-16 17:29:14 +0900

    Change Type AbandonedPets? -> List<AbandonedPets>

--- data/src/main/java/com/project/data/repository/AbandonedPetsRepositoryImpl.kt
+++ data/src/main/java/com/project/data/repository/AbandonedPetsRepositoryImpl.kt
@@ -6,7 +6,7 @@ import com.project.domain.model.AbandonedPets
 import com.project.domain.repository.AbandonedPetsRepository
 
 class AbandonedPetsRepositoryImpl(private val dataSource: AbandonedPetsDataSource) : AbandonedPetsRepository {
-    override suspend fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): AbandonedPets? {
+    override suspend fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): List<AbandonedPets> {
         return AbandonedPetsMapper.mapperToAbandonedPets(dataSource.getInfo(bgnde, endde, pageNo, numOfRows))
     }
 }
--- domain/src/main/java/com/project/domain/usecase/GetInfoUseCase.kt
+++ domain/src/main/java/com/project/domain/usecase/GetInfoUseCase.kt
@@ -4,7 +4,7 @@ import com.project.domain.model.AbandonedPets
 import com.project.domain.repository.AbandonedPetsRepository
 
 class GetInfoUseCase(private val repository: AbandonedPetsRepository) {
-    suspend operator fun invoke(bgnde: String, endde: String, pageNo: String, numOfRows: String): AbandonedPets? {
+    suspend operator fun invoke(bgnde: String, endde: String, pageNo: String, numOfRows: String): List<AbandonedPets> {
         return repository.getInfo(bgnde, endde, pageNo, numOfRows)
     }
 }
