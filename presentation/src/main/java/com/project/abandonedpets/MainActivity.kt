d306dfdf44ba4ad5193519fd389781a589c72177
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-16 17:29:54 +0900

    Call GetInfo

--- presentation/src/main/java/com/project/abandonedpets/MainActivity.kt
+++ presentation/src/main/java/com/project/abandonedpets/MainActivity.kt
@@ -2,10 +2,22 @@ package com.project.abandonedpets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.project.abandonedpets.databinding.ActivityMainBinding
+import com.project.abandonedpets.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+        viewModel.getInfo("20220606", "20220614", "1", "10")
     }
 }
