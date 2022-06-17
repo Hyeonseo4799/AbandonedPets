fdbedacadfc48d45f8730625d1f8da518637595d
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-17 14:22:33 +0900

    RecyclerView 설정

--- presentation/src/main/java/com/project/abandonedpets/MainActivity.kt
+++ presentation/src/main/java/com/project/abandonedpets/MainActivity.kt
@@ -3,11 +3,12 @@ package com.project.abandonedpets
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.abandonedpets.databinding.ActivityMainBinding
 import com.project.abandonedpets.viewmodel.MainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.project.domain.model.AbandonedPets
+import com.project.domain.model.PageInfo
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
-        viewModel.getInfo("20220606", "20220614", "1", "10")
+        val adapter = setRec()
+        val abandonedPetsObserver = Observer<List<AbandonedPets>> {
+            adapter.apply {
+                abandonedPetsList = it
+                notifyDataSetChanged()
+            }
+        }
+        val pageInfoObserver = Observer<PageInfo> { binding.page = it }
+
+        viewModel.getInfo("20220606", "20220614", "1", "50").observe(this@MainActivity, abandonedPetsObserver)
+        viewModel.getPageInfo("20220606", "20220614", "1", "10").observe(this@MainActivity, pageInfoObserver)
+    }
+
+    private fun setRec(): MainAdapter {
+        val adapter = MainAdapter(this@MainActivity)
+        binding.recyclerview.adapter = adapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(this@MainActivity)
+        return adapter
     }
 }
---
+++ presentation/src/main/java/com/project/abandonedpets/MainAdapter.kt
@@ -0,0 +1,53 @@
+package com.project.abandonedpets
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.project.abandonedpets.databinding.ListItemBinding
+import com.project.domain.model.AbandonedPets
+
+class MainAdapter(private val context: Context) : RecyclerView.Adapter<MainAdapter.MainVH>() {
+    var abandonedPetsList = listOf<AbandonedPets>()
+
+    inner class MainVH(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: AbandonedPets) {
+            data.petGender = checkGender(data.petGender)
+            data.neuterState = checkNeuterState(data.neuterState)
+
+            binding.data = data
+            Glide.with(context).load(data.petImg).into(binding.thumbnail)
+            binding.thumbnail.clipToOutline = true
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainVH {
+        val binding = ListItemBinding.inflate(LayoutInflater.from(context), parent, false)
+        return MainVH(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainVH, position: Int) {
+        holder.onBind(abandonedPetsList[position])
+    }
+
+    override fun getItemCount(): Int = abandonedPetsList.size
+
+    fun checkGender(gender: String): String {
+        return when (gender) {
+            "M" -> "수컷"
+            "F" -> "암컷"
+            "Q" -> "미상"
+            else -> ""
+        }
+    }
+
+    fun checkNeuterState(neuterState: String): String {
+        return when (neuterState) {
+            "Y" -> "예"
+            "N" -> "아니오"
+            "U" -> "미상"
+            else -> ""
+        }
+    }
+}
