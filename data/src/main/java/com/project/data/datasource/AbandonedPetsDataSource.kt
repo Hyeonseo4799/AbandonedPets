409923d792b4778658931d6368bb5c5101ab25b7
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-21 21:16:30 +0900

    AbandonedPetsApi의 getInfo 기본값 제거 & DataSource에서 기본값 설정

--- data/src/main/java/com/project/data/datasource/AbandonedPetsDataSource.kt
+++ data/src/main/java/com/project/data/datasource/AbandonedPetsDataSource.kt
@@ -3,5 +3,5 @@ package com.project.data.datasource
 import com.project.data.model.AbandonedPetsResponse
 
 interface AbandonedPetsDataSource {
-    suspend fun getInfo(bgnde: String, endde: String): AbandonedPetsResponse
+    suspend fun getInfo(bgnde: String, endde: String, page: String = "1", numOfRows: String = "10"): AbandonedPetsResponse
 }
--- data/src/main/java/com/project/data/datasource/AbandonedPetsDataSourceImpl.kt
+++ data/src/main/java/com/project/data/datasource/AbandonedPetsDataSourceImpl.kt
@@ -5,7 +5,7 @@ import com.project.data.model.AbandonedPetsResponse
 import com.project.data.utils.base.BaseRepository
 
 class AbandonedPetsDataSourceImpl(private val api: AbandonedPetsApi) : BaseRepository(), AbandonedPetsDataSource {
-    override suspend fun getInfo(bgnde: String, endde: String): AbandonedPetsResponse {
-        return safeApiCall { api.getInfo(bgnde, endde).body() }!!
+    override suspend fun getInfo(bgnde: String, endde: String, page: String, numOfRows: String): AbandonedPetsResponse {
+        return safeApiCall { api.getInfo(bgnde, endde, page, numOfRows).body() }!!
     }
 }
