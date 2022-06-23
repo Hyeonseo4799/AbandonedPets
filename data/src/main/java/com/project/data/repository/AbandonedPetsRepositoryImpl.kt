66c505f3bf60c5a971a7d26ab872bc0422db8d75
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-23 20:38:42 +0900

    Paging3를 이용한 무한스크롤 구현

--- data/src/main/java/com/project/data/repository/AbandonedPetsRepositoryImpl.kt
+++ data/src/main/java/com/project/data/repository/AbandonedPetsRepositoryImpl.kt
@@ -1,17 +1,29 @@
 package com.project.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.project.data.datasource.AbandonedPetsDataSource
+import com.project.data.datasource.AbandonedPetsPagingSource
 import com.project.data.mapper.AbandonedPetsMapper
 import com.project.domain.model.AbandonedPets
 import com.project.domain.model.PageInfo
 import com.project.domain.repository.AbandonedPetsRepository
+import kotlinx.coroutines.flow.Flow
 
 class AbandonedPetsRepositoryImpl(private val dataSource: AbandonedPetsDataSource) : AbandonedPetsRepository {
-    override suspend fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): List<AbandonedPets> {
-        return AbandonedPetsMapper.mapperToAbandonedPets(dataSource.getInfo(bgnde, endde, pageNo, numOfRows))
+    override suspend fun getInfo(bgnde: String, endde: String, page: String, numOfRows: String): List<AbandonedPets> {
+        return AbandonedPetsMapper.mapperToAbandonedPets(dataSource.getInfo(bgnde, endde, page, numOfRows))
     }
 
-    override suspend fun getPageInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): PageInfo {
-        return AbandonedPetsMapper.mapperToPageInfo(dataSource.getInfo(bgnde, endde, pageNo, numOfRows))
+    override suspend fun getPageInfo(bgnde: String, endde: String): PageInfo {
+        return AbandonedPetsMapper.mapperToPageInfo(dataSource.getInfo(bgnde, endde))
+    }
+
+    override suspend fun getInfoByPaging(bgnde: String, endde: String): Flow<PagingData<AbandonedPets>> {
+        return Pager(
+            config = PagingConfig(pageSize = 50),
+            pagingSourceFactory = { AbandonedPetsPagingSource(this, bgnde, endde) }
+        ).flow
     }
 }
--- domain/src/main/java/com/project/domain/repository/AbandonedPetsRepository.kt
+++ domain/src/main/java/com/project/domain/repository/AbandonedPetsRepository.kt
@@ -1,9 +1,12 @@
 package com.project.domain.repository
 
+import androidx.paging.PagingData
 import com.project.domain.model.AbandonedPets
 import com.project.domain.model.PageInfo
+import kotlinx.coroutines.flow.Flow
 
 interface AbandonedPetsRepository {
-    suspend fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): List<AbandonedPets>
-    suspend fun getPageInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): PageInfo
+    suspend fun getInfo(bgnde: String, endde: String, page: String, numOfRows: String) : List<AbandonedPets>
+    suspend fun getPageInfo(bgnde: String, endde: String): PageInfo
+    suspend fun getInfoByPaging(bgnde: String, endde: String): Flow<PagingData<AbandonedPets>>
 }
