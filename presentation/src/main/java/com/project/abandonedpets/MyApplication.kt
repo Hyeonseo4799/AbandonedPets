81bfdd179cd8296918937d9ddde9aa6b63e625d2
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-28 08:49:14 +0900

    DataStore 객체 생성

--- presentation/src/main/java/com/project/abandonedpets/MyApplication.kt
+++ presentation/src/main/java/com/project/abandonedpets/MyApplication.kt
@@ -5,11 +5,21 @@ import com.project.abandonedpets.di.*
 import org.koin.core.context.startKoin
 
 class MyApplication : Application() {
+    private lateinit var dataStore : DataStoreRadioValue
+
+    companion object {
+        private lateinit var myApplication: MyApplication
+        fun getInstance(): MyApplication = myApplication
+    }
+
     override fun onCreate() {
         super.onCreate()
-
+        myApplication = this@MyApplication
         startKoin {
-            modules(abandonedPetsDataSourceModule, abandonedPetsRepositoryModule, getInfoUseCaseModule, getPageInfoUseCase, viewModelModule)
+            modules(abandonedPetsDataSourceModule, abandonedPetsRepositoryModule, getPageInfoUseCase, viewModelModule, abandonedPetsRepositoryImpl)
         }
+        dataStore = DataStoreRadioValue(this@MyApplication)
     }
+
+    fun getDataStore(): DataStoreRadioValue = dataStore
 }
