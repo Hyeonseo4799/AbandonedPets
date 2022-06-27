057060d64e410e5e654649ccd7900298070f4eff
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-06-28 08:47:46 +0900

    무한 스크롤 기능을 Paging3로 대체

--- presentation/src/main/java/com/project/abandonedpets/di/GetInfoUseCaseModule.kt
+++
@@ -1,8 +0,0 @@
-package com.project.abandonedpets.di
-
-import com.project.domain.usecase.GetInfoUseCase
-import org.koin.dsl.module
-
-val getInfoUseCaseModule = module {
-    single { GetInfoUseCase(get()) }
-}
