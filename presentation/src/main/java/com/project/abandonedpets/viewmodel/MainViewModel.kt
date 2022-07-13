ca9afb35ad1f6b3ace9b13dca137ee80e80ce200
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-07-13 11:21:26 +0900

    코드 간소화

--- presentation/src/main/java/com/project/abandonedpets/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/project/abandonedpets/viewmodel/MainViewModel.kt
@@ -4,28 +4,30 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.project.data.repository.AbandonedPetsRepositoryImpl
 import com.project.domain.model.AbandonedPets
 import com.project.domain.model.PageInfo
-import com.project.domain.usecase.GetInfoUseCase
 import com.project.domain.usecase.GetPageInfoUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel(private val getInfoUseCase: GetInfoUseCase, private val getPageInfoUseCase: GetPageInfoUseCase) : ViewModel() {
-    fun getInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): LiveData<List<AbandonedPets>> {
-        val abandonedPetsList = MutableLiveData<List<AbandonedPets>>()
-        viewModelScope.launch {
-            abandonedPetsList.value = getInfoUseCase(bgnde, endde, pageNo, numOfRows)
-        }
-        return abandonedPetsList
-    }
-
-    fun getPageInfo(bgnde: String, endde: String, pageNo: String, numOfRows: String): LiveData<PageInfo> {
+class MainViewModel(private val getPageInfoUseCase: GetPageInfoUseCase, private val abandonedPetsRepositoryImpl: AbandonedPetsRepositoryImpl) :
+    ViewModel() {
+    fun getPageInfo(bgnde: String, endde: String): LiveData<PageInfo> {
         val pageInfoList = MutableLiveData<PageInfo>()
         viewModelScope.launch {
-            pageInfoList.value = getPageInfoUseCase(bgnde, endde, pageNo, numOfRows)
+            pageInfoList.value = getPageInfoUseCase(bgnde, endde)
         }
         return pageInfoList
     }
 
-
+    suspend fun getInfo(beginDate: String, endDate: String): Flow<PagingData<AbandonedPets>> {
+        val pagingData = withContext(Dispatchers.IO) {
+            abandonedPetsRepositoryImpl.getInfoByPaging(beginDate, endDate)
+        }
+        return pagingData
+    }
 }
